e32cd3d01e23a341965ff3aad1645369921898f9
PedroTwoBananas <87867356+PedroTwoBananas@users.noreply.github.com>
2022-08-09 13:40:55 +0300

    Редактирование задач 10 и 11

--- 11/task11.js
+++ 11/task11.js
@@ -1,18 +1,20 @@
-// const asyncTimeout = (timeout) => {
-//   return new Promise((resolve) => setTimeout(resolve, timeout));
-// };
-//
-// const promiseStack = (promises, quantity) => {};
-//
-// promiseStack(
-//   [
-//     () => asyncTimeout(4000).then(() => console.log(1)),
-//     () => asyncTimeout(4000).then(() => console.log(1)),
-//     () => asyncTimeout(2000).then(() => console.log(2)),
-//     () => asyncTimeout(2000).then(() => console.log(2)),
-//     () => asyncTimeout(1000).then(() => console.log(3)),
-//     () => asyncTimeout(1000).then(() => console.log(3)),
-//     () => asyncTimeout(3000).then(() => console.log(4)),
-//   ],
-//   2
-// );
+const asyncTimeout = (timeout) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
+const promiseStack = (promises) => {
+  promises.reduce((acc, fn) => acc.then(fn), Promise.resolve());
+};
+
+promiseStack(
+  [
+    () => asyncTimeout(4000).then(() => console.log(1)),
+    () => asyncTimeout(4000).then(() => console.log(1)),
+    () => asyncTimeout(2000).then(() => console.log(2)),
+    () => asyncTimeout(2000).then(() => console.log(2)),
+    () => asyncTimeout(1000).then(() => console.log(3)),
+    () => asyncTimeout(1000).then(() => console.log(3)),
+    () => asyncTimeout(3000).then(() => console.log(4)),
+  ],
+  2
+);
